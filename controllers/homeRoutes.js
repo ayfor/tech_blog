dec4632133084251921bc7937357b4d8a3d2d857
ayfor <josh.a.stubbington@gmail.com>
2021-07-05 07:38:40 -0400

    added signup page and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,21 +2,29 @@ const router = require('express').Router();
 // const { Project, User } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.redirect('/login');
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+  // else
+  // {
+  //   res.redirect('/login');
+  // }
+  
 });
 
 router.get('/project/:id', async (req, res) => {
  
 });
 
-router.get('/profile', async (req, res) => {
-  
+router.get('/signup', async (req, res) => {
+  res.render('signup')
 });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   // if (req.session.logged_in) {
-  //   res.redirect('/profile');
+  //   res.redirect('/dashboard');
   //   return;
   // }
 
